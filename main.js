31c28ed09787ab8d006a500da984e3fcff2b6043
Le Huynh <lhuynh100@gmail.com>
2022-10-15 14:38:11 -0500

    add card data to grab datas and assign to correct variables

--- main.js
+++ main.js
@@ -19,3 +19,20 @@ let getPokeData = () => {
     generateCard(data);
 });
 };
+
+// Generate Card
+
+let generateCard = (data) => {
+  // Grab necessary data and assign it to variables
+
+  console.log(data);
+  const hp = data.stats[0].base_stat;
+  const imgSrc = data.sprites.other.dream_world.front_default;
+  const pokeName = data.name[0].toUpperCase() + data.name.slice(1);
+  const statAttack = data.stats[1].base_stat;
+  const statDefense = data.stats[2].base_stat;
+  const statSpeed = data.stats[5].base_stat;
+
+  // Set themeColor based on pokemon type
+  const themeColor = typeColor[data.types[0].type.name];
+  console.log(themeColor);
